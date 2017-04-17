7955c8cefe4f49603bcbf8b8f91a1f539e053899
dannielccw <dannielccw@gmail.com>
2017-04-17 15:19:23 +0800

    換用Gson library來解析JSON檔 - put into listview

--- app/src/main/java/com/example/yvtc/s041701/MainActivity.java
+++ app/src/main/java/com/example/yvtc/s041701/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.yvtc.s041701;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
@@ -19,11 +21,13 @@ import org.json.JSONObject;
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
-
+    ArrayAdapter<String> adapter;
+    ListView lv;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        lv = (ListView) findViewById(R.id.listView);
 
         RequestQueue queue = Volley.newRequestQueue(MainActivity.this);
         StringRequest request = new StringRequest("http://data.ntpc.gov.tw/od/data/api/BF90FA7E-C358-4CDA-B579-B6C84ADC96A1?$format=json",
@@ -33,11 +37,14 @@ public class MainActivity extends AppCompatActivity {
                     Log.d("NET", response);
                     Gson gson = new Gson();
                     ArrayList<Animal> mylist = gson.fromJson(response, new TypeToken<ArrayList<Animal>>() {}.getType());
+                    ArrayList<String> strlist = new ArrayList<>();
                     for (Animal a : mylist)
                     {
                         Log.d("NET", a.district);
+                        strlist.add(a.district);
                     }
-
+                    adapter = new ArrayAdapter<String>(MainActivity.this, android.R.layout.simple_list_item_1, strlist);
+                    lv.setAdapter(adapter);
                 }
             }, new Response.ErrorListener() {
             @Override
